a7b3bde6679365b60712dc3d3c1bd7b2b67d1094
Os24 <oscarhuang950324@gmail.com>
2024-10-07 14:44:02 +0800

    pages problem

---
+++ src/pages/session.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from '@/components/navbar'
+import Footer from '@/components/footer'
+export default function session() {
+  return (
+    <div>
+        <Navbar/>
+        <Footer/>
+    </div>
+  )
+}
